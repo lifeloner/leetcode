c26b21c3e9fb6cfc168d61d74933de634aa0fcf2
坚忍 <fy165065@alipay.com>
2019-01-12 18:21:38 +0800

    训练

--- src/main/java/start/Solution.java
+++ src/main/java/start/Solution.java
@@ -610,12 +610,53 @@ public class Solution {
         return max;
     }
 
+    // DP
+    public static boolean isIdealPermutation(int[] A) {
+        if (A == null || A.length <= 0) {
+            return true;
+        }
+        int min = A[A.length - 1];
+        for (int i = A.length - 3; i >= 0; i--) {
+            if (A[i] > min) {
+                return false;
+            }
+            min = Math.min(min, A[i + 1]);
+        }
+        return true;
+    }
+
+    public static boolean canTransform(String start, String end) {
+        return true;
+    }
+
+    public static int kthGrammar(int N, int K) {
+        if (N == 1) {
+            return 0;
+        }
+        boolean[] flag = new boolean[N];
+        int count = N, m;
+        while (count != 1) {
+            m = K / 2;
+            if (K % 2 != 0) {
+                flag[count - 1] = true;
+                m++;
+            }
+            K = m;
+            count--;
+        }
+        m = 0;
+        for (int i = 1; i < N; i++) {
+            if (m == 0) {
+                m = flag[i] ? 0 : 1;
+            } else {
+                m = flag[i] ? 1 : 0;
+            }
+        }
+        return m;
+    }
+
     public static void main(String[] args) {
-        int[][] arrays = new int[][]
-                {{16, 18}, {11, 18}, {15, 23}, {1, 16}, {10, 16}, {6, 19}, {18, 20}, {7, 19}, {10, 11}, {11, 23}, {6, 7}, {23, 25},
-                        {1, 3},
-                        {7, 12}, {1, 13}, {23, 25}, {10, 22}, {23, 25}, {0, 19}, {0, 13}, {7, 12}, {14, 19}, {8, 17}, {7, 23}, {4, 24}};
-        System.out.println(intersectionSizeTwo(arrays));
+        System.out.println(kthGrammar(4,5));
 
     }
 
